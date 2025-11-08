f8f5463ca3c64236536e17fda6dee4a5654a77d5
karthikeyan799 <akarthikeyan799@gmail.com>
2025-11-08 15:31:45 +0530

    new

--- src/API.js
+++ src/API.js
@@ -1,4 +1,5 @@
 export const API_BASE_LINE=
-// "https://springprojectinsurance-production-87a1.up.railway.app/"//old insurance
-"https://disciplined-generosity-production-adb0.up.railway.app/" //new insurance
+// "https://springprojectinsurance-production-87a1.up.railway.app/"//old insurance db
+"https://disciplined-generosity-production-adb0.up.railway.app/" //new insurance db disciplined
+// "https://springprojectinsurance-production.up.railway.app/" // new new one
 // "http://localhost:8080/"
