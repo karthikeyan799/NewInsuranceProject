64951911e63c594a1625fb5d975c2ae5382aa902
karthikeyan799 <akarthikeyan799@gmail.com>
2025-11-08 10:33:44 +0530

    back to new insu db

--- src/API.js
+++ src/API.js
@@ -1,3 +1,4 @@
-export const API_BASE_LINE="https://springprojectinsurance-production-87a1.up.railway.app/"
-// " https://disciplined-generosity-production-adb0.up.railway.app/" 
+export const API_BASE_LINE=
+// "https://springprojectinsurance-production-87a1.up.railway.app/"
+"https://disciplined-generosity-production-adb0.up.railway.app/" 
 // ,"http://localhost:8080/"
